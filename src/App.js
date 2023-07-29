b500deb09143c12acc3812cf0ea7342b0e2a509a
Shivam Tyagi <shivamtyagivivoy55l@gmail.com>
2023-07-30 00:14:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react'
-// import Resturant from './component/basics/Resturant';
-import UseState from './component/Hooks/useState';
-// const App = () => {
-//   return (
-//     <Resturant/> 
-//   )
-// }
-
-// export default App
-
+import Resturant from './component/basics/Resturant';
+// import UseState from './component/Hooks/useState';
 const App = () => {
   return (
-    <UseState/> 
+    <Resturant/> 
   )
 }
 
 export default App
+
+// const App = () => {
+//   return (
+//     <UseState/> 
+//   )
+// }
+
+// export default App
